7d2dd945869331e6072ccb343250795cfeb39e3a
Alex Carlin <carlin@ucdavis.edu>
2015-08-21 20:43:47 -0700

    Completely working on 3orf designs

--- order.bash
+++ order.bash
@@ -1,8 +1,8 @@
 export SCAFFOLD='private/3orf.fasta'
-for i in ~/Documents/boxi/etc/3orf_sandbox/*des*pdb; do 
+for i in example/3orf_sandbox/*des*pdb; do 
   export alex_f="${i/pdb/fasta}"
   python pdb_to_fasta.py $i > $alex_f
   blastx -query ${SCAFFOLD} -subject $alex_f -outfmt "6 qseq sseq length qstart qend" | python diff.py  
 done |  python transcriptic_format.py > kunkel_mutants.csv 
 
-python kunkel_mutagenesis.py 
+python kunkel.py params.json | transcriptic analyze 
